a148684ceff90690f537663488883c6f7eb2d49c
benoni <benoni.silva@gmail.com>
2017-05-08 08:40:14 -0300

    pdf viewer

--- www/templates/ferramentas/config/config-service.js
+++ www/templates/ferramentas/config/config-service.js
@@ -20,7 +20,7 @@
             }
 
             function get() {
-                return "http://localhost:1119";
+                return "http://187.33.231.170:8080/procon-mobile";
             }
         }
 
