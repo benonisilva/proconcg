b8ce5f8aa0f3a417f26d00d80662eb19821bc7a0
benoni <benoni.silva@gmail.com>
2017-07-14 10:00:16 -0300

    icon

--- tools/logcat.bat
+++ tools/logcat.bat
@@ -1,1 +1,5 @@
 adb logcat | findstr /R /C:"CONSOLE";
+"C:\Users\benon\AppData\Local\Android\android-sdk\build-tools\25.0.2\zipalign" -v 4 "C:/Users/benon/Development/procon-web/proconcg/platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk" ProconCGMovel1.apk
+cordova build --release android
+"C:\Program Files\Java\jdk1.8.0_131\bin\jarsigner" -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore C:\Users\benon\Development\procon-web\lb_key.keystore "C:/Users/benon/Development/procon-web/proconcg/platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk" lb_key
+"C:\Program Files\Java\jdk1.8.0_131\bin\keytool"
