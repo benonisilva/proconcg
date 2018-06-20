8506c3bb49b8d02f281dfd4abae3a7a22e650538
benonisilva <benonisilva@hotmail.com>
2018-06-20 10:19:07 -0300

    novo servidor ver 0.3.0

--- www/templates/ferramentas/config/config-service.js
+++ www/templates/ferramentas/config/config-service.js
@@ -3,21 +3,18 @@
     angular
         .module('starter.services')
         .factory('ConfigService', ConfigService);         
-
         function ConfigService() {
             var config = {
                 set : set,
                 get : get
             };
             return config;
-
             function set(url) {
                 var key = "_url";
                 return;
             }
-
             function get() {
-                return "http://191.253.16.179:8080/procon-mobile";
+                return "http://191.253.16.180:8080/procon-mobile";
             }
         }
 
